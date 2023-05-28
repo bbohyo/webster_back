f5c747debc57cdde71d46634a1541f1990bd0a79
minyou2675 <kymyy2675@gmail.com>
2023-05-28 17:45:44 +0900

    add mail depend

--- src/main/java/webster/back/webster/back/api/MemberApiController.java
+++ src/main/java/webster/back/webster/back/api/MemberApiController.java
@@ -1,21 +1,21 @@
 package webster.back.webster.back.api;
 
+import jakarta.servlet.http.HttpSession;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import webster.back.webster.back.domain.GenderStatus;
 import webster.back.webster.back.domain.Member;
 import webster.back.webster.back.service.MemberService;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 public class MemberApiController {
     private final MemberService memberservice;
 
-    @PostMapping("api/join")
+    @PostMapping("/join")
     public CreateMemberResponse join(@RequestBody CreateMemberRequest request){
         Member member = new Member();
         member.setName(request.name);
@@ -26,11 +26,20 @@ public class MemberApiController {
         return new CreateMemberResponse();
     }
 
-    @PostMapping("api/login")
-    public SignUpMemberResponse login (@RequestBody SignUpMemberRequest request){
-        memberservice.login(request.email, request.password);
+    @PostMapping("/login")
+    public SignUpMemberResponse login (@RequestBody SignUpMemberRequest request, @RequestParam HttpSession session){
+        List<Member> loginresult = memberservice.login(request.email, request.password);
+        if (loginresult.isEmpty()){
+            System.out.println("failed");
+        }
+        else{
+            session.setMaxInactiveInterval(60*60*8);
+            session.setAttribute("email",request.email);
+            session.setAttribute("loginok","yes");
 
-        return new SignUpMemberResponse();
+        }
+
+        return new SignUpMemberResponse(request);
     }
 
     @Data
@@ -57,6 +66,11 @@ public class MemberApiController {
     static class SignUpMemberResponse{
         private String email;
         private String password;
+
+        public SignUpMemberResponse(SignUpMemberRequest request){
+            this.email = request.email;
+            this.password = request.password;
+        }
     }
 
 
